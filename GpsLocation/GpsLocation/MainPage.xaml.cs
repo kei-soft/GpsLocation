2230bb461b4737fa98ec6422b952e6a10341bb23
준 강 <kjun@kohyoung.com>
2021-05-21 13:00:02 +0900

    이미지 버튼 추가 수정 기능 추가

--- GpsLocation/GpsLocation/MainPage.xaml.cs
+++ GpsLocation/GpsLocation/MainPage.xaml.cs
@@ -50,27 +50,10 @@ namespace GpsLocation
         }
         #endregion
         #region DeleteButton_Clicked
-        private async void DeleteButton_Clicked(object sender, EventArgs e)
+        private void DeleteButton_Clicked(object sender, EventArgs e)
         {
             string name = ((Button)sender).ClassId;
-
-            bool isYes = await DisplayAlert("삭제확인", "'" + name + "' 항목을 제거 하시겠습니까?", "예", "아니오");
-            if (isYes)
-            {
-                var deleteitem = locationInfos.Where(c => c.Name == name).FirstOrDefault();
-                if (deleteitem != null)
-                {
-                    locationInfos.Remove(deleteitem);
-
-                    SetPreferences(this.SaveKey, JsonConvert.SerializeObject(locationInfos));
-
-                    this.locationStackLayout.Children.Clear();
-                    foreach (var locationInfo in this.locationInfos)
-                    {
-                        AddLocationData(locationInfo);
-                    }
-                }
-            }
+            DeleteAction(name);
         }
         #endregion
         #region ClearButton_Clicked
@@ -92,11 +75,11 @@ namespace GpsLocation
         /// </summary>
         private async void GetLocation()
         {
-            string result = await DisplayPromptAsync("위치이름", "위치이름을 지정해주세요");
+            string result = await DisplayPromptAsync("Location Name", "Input Location Name");
 
             if (locationInfos.Where(c => c.Name == result).Any())
             {
-                await DisplayAlert("이름중복", "위치 이름이 중복됩니다.", "예");
+                await DisplayAlert("Location Name Duplicate", "Duplicate location name.", "OK");
                 return;
             }
 
@@ -159,23 +142,107 @@ namespace GpsLocation
         private void AddLocationData(LocationInfo locationInfo)
         {
             StackLayout stackLayout = new StackLayout() { Orientation = StackOrientation.Horizontal, HeightRequest = 50};
-            Label nameLabel = new Label() { TextColor = Color.Black, Text = locationInfo.Name, WidthRequest = 70, Margin = new Thickness(10, 0, 0, 0), VerticalOptions = LayoutOptions.Center, VerticalTextAlignment = TextAlignment.Center };
-            Label latLabel = new Label() { TextColor = Color.Black, Text = "Lat: " + locationInfo.Latitude, VerticalOptions = LayoutOptions.Center, VerticalTextAlignment = TextAlignment.Center };
-            Label lonLabel = new Label() { TextColor = Color.Black, Text = "Lon: " + locationInfo.Longitude, VerticalOptions = LayoutOptions.Center, VerticalTextAlignment = TextAlignment.Center };
+            
+            Image editImage = new Image()
+            {
+                Source = "EditImage.png",
+                HorizontalOptions = LayoutOptions.Start,
+                VerticalOptions = LayoutOptions.Center,
+                WidthRequest = 20,
+                HeightRequest = 20,
+                Margin = new Thickness(0, 0, 5, 0),
+                BackgroundColor = Color.White,
+                ClassId = locationInfo.Name
+            };
+
+            TapGestureRecognizer editTapGestureRecognizer = new TapGestureRecognizer();
+            editTapGestureRecognizer.Tapped += async (s, e) =>
+            {
+                string name = ((Image)s).ClassId;
+
+                string result = await DisplayPromptAsync("Edit Location Name", "Input Location Name", initialValue: name);
+
+                if (result != null && result != name)
+                {
+                    if (locationInfos.Where(c => c.Name == result).Any())
+                    {
+                        await DisplayAlert("Location Name Duplicate", "Duplicate Location Name.", "OK");
+                        return;
+                    }
+                    else
+                    {
+                        var editItem = locationInfos.Where(c => c.Name == name).FirstOrDefault();
+
+                        if (editItem != null)
+                        {
+                            editItem.Name = result;
+                            SetPreferences(this.SaveKey, JsonConvert.SerializeObject(locationInfos));
+
+                            this.locationStackLayout.Children.Clear();
+                            foreach (var item in this.locationInfos)
+                            {
+                                AddLocationData(item);
+                            }
+                        }
+                    }
+                }
+            };
+
+            editImage.GestureRecognizers.Add(editTapGestureRecognizer);
+
+            TapGestureRecognizer copyTapGestureRecognizer = new TapGestureRecognizer();
+            copyTapGestureRecognizer.Tapped += (s, e) =>
+            {
+                string clipdata = $"{locationInfo.Name} = lat : {locationInfo.Latitude}, lon : {locationInfo.Latitude}";
+                Clipboard.SetTextAsync(clipdata);
+                DisplayAlert("Copy Complete", clipdata, "OK");
+            };
+
+            Label nameLabel = new Label() { TextColor = Color.Black, Text = locationInfo.Name, WidthRequest = 70, Margin = new Thickness(10, 0, 0, 0), VerticalOptions = LayoutOptions.Center, VerticalTextAlignment = TextAlignment.Center, FontSize = 14, FontAttributes = FontAttributes.Bold };
+            Label latLabel = new Label() { TextColor = Color.Black, Text = "Lat: " + locationInfo.Latitude, VerticalOptions = LayoutOptions.Center, VerticalTextAlignment = TextAlignment.Center, FontSize = 15 };
+            Label lonLabel = new Label() { TextColor = Color.Black, Text = "Lon: " + locationInfo.Longitude, VerticalOptions = LayoutOptions.Center, VerticalTextAlignment = TextAlignment.Center, FontSize = 15 };
+            nameLabel.GestureRecognizers.Add(copyTapGestureRecognizer);
+            latLabel.GestureRecognizers.Add(copyTapGestureRecognizer);
+            lonLabel.GestureRecognizers.Add(copyTapGestureRecognizer);
+
             stackLayout.Children.Add(nameLabel);
+            stackLayout.Children.Add(editImage);
             stackLayout.Children.Add(latLabel);
             stackLayout.Children.Add(lonLabel);
-            Button deleteButton = new Button() 
-            { 
+
+            Image image = new Image()
+            {
+                Source = "DeleteImage.png",
                 HorizontalOptions = LayoutOptions.EndAndExpand,
                 VerticalOptions = LayoutOptions.Center,
-                WidthRequest = 40, HeightRequest = 40, Text = "x", Margin = new Thickness(0, 0, 5, 0), 
-                BackgroundColor = Color.White, BorderColor = Color.Black, TextColor = Color.Black, BorderWidth = 0, ClassId= locationInfo.Name
+                WidthRequest = 20,
+                HeightRequest = 20,
+                Margin = new Thickness(0, 0, 5, 0),
+                BackgroundColor = Color.White,
+                ClassId = locationInfo.Name
             };
-            deleteButton.Clicked += DeleteButton_Clicked;
-            stackLayout.Children.Add(deleteButton);
+
+            TapGestureRecognizer deleteTapGestureRecognizer = new TapGestureRecognizer();
+            deleteTapGestureRecognizer.Tapped += (s,e) =>
+            {
+                string name = ((Image)s).ClassId;
+                DeleteAction(name);
+            };
+
+            image.GestureRecognizers.Add(deleteTapGestureRecognizer);
+
+            //Button deleteButton = new Button() 
+            //{ 
+            //    HorizontalOptions = LayoutOptions.EndAndExpand,
+            //    VerticalOptions = LayoutOptions.Center,
+            //    WidthRequest = 40, HeightRequest = 40, Text = "x", Margin = new Thickness(0, 0, 5, 0), 
+            //    BackgroundColor = Color.White, BorderColor = Color.Black, TextColor = Color.Black, BorderWidth = 0, ClassId= locationInfo.Name
+            //};
+            //deleteButton.Clicked += DeleteButton_Clicked;
+            stackLayout.Children.Add(image);
             this.locationStackLayout.Children.Add(stackLayout);
         }
+
         #endregion
         #region SetPreferences
         /// <summary>
@@ -198,6 +265,33 @@ namespace GpsLocation
         public string GetPreferences(string key, string defaultValue = null)
         {
             return Preferences.Get(key, defaultValue);
+        }
+        #endregion
+
+        #region DeleteAction
+        /// <summary>
+        /// 아이템을 삭제합니다.
+        /// </summary>
+        /// <param name="name"></param>
+        private async void DeleteAction(string name)
+        {
+            bool isYes = await DisplayAlert("Delete", "'" + name + "' Do you want to delete?", "Yes", "No");
+            if (isYes)
+            {
+                var deleteitem = locationInfos.Where(c => c.Name == name).FirstOrDefault();
+                if (deleteitem != null)
+                {
+                    locationInfos.Remove(deleteitem);
+
+                    SetPreferences(this.SaveKey, JsonConvert.SerializeObject(locationInfos));
+
+                    this.locationStackLayout.Children.Clear();
+                    foreach (var locationInfo in this.locationInfos)
+                    {
+                        AddLocationData(locationInfo);
+                    }
+                }
+            }
         }
         #endregion
     }
